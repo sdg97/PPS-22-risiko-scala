d9729eb3b833a328f6ca565f10cdc5a7a1c3f376
Pietro Lelli <pietro.lelli00@gmail.com>
2023-07-11 15:21:32 +0200

    fixed button's click action

--- src/main/scala/view/GameMapGui.scala
+++ src/main/scala/view/GameMapGui.scala
@@ -20,7 +20,7 @@ object GameMapGui extends App {
 
   val model = new ModelImpl()
   val controller = new Controller(model)
-  val buttonMap: mutable.Map[String, JButton] = mutable.Map()
+  val buttonMap: mutable.Map[String, JButtonExtended] = mutable.Map()
 
   // Carica l'immagine di sfondo
   val backgroundImage: Image = javax.imageio.ImageIO.read(new java.io.File("src/main/resources/img_map.png"))
@@ -57,13 +57,14 @@ object GameMapGui extends App {
       val posX = parts(1).trim
       val posY = parts(2).trim
 
-      val btnState = new JButton() {
+      val btnState = new JButtonExtended("") {
         setBorder(BorderFactory.createEmptyBorder())
         setContentAreaFilled(false) // Rimuove lo sfondo del bottone
         setForeground(Color.BLACK) // Imposta il colore del testo
         setFocusPainted(false) // Rimuove l'effetto di focuss
         setText(name)
         setFont(new Font("Arial", 12, 10))
+        setRolloverEnabled(true)
 
         override def paintComponent(g: Graphics): Unit = {
           val g2d = g.asInstanceOf[Graphics2D]
@@ -77,186 +78,46 @@ object GameMapGui extends App {
       }
 
       btnState.addMouseListener(
-
         new MouseAdapter() {
         override def mouseEntered(evt: MouseEvent): Unit = {
-          if(!btnState.isSelected)
+          if(!btnState.isSelected && !btnState.isNeighbour)
             btnState.setBorder(javax.swing.BorderFactory.createLineBorder(Color.BLACK, 2))
         }
 
         override def mouseExited(evt: MouseEvent): Unit = {
-          if(!btnState.isSelected)
+          if(!btnState.isSelected && !btnState.isNeighbour)
             btnState.setBorder(BorderFactory.createEmptyBorder())
         }
       })
 
       btnState.addActionListener((_: ActionEvent) => {
-        if (!btnState.isSelected) {
+        if(btnState.isNeighbour) {
+          //TODO invoke attack method
+          println("attack")
+          resetButton()
+        }
+        else if(btnState.isSelected) {
+          resetButton()
+        }
+        else {
+          resetButton()
           val neighbors: Set[String] = controller.getNeighbor(name, controller.getCurrentPlayer())
           neighbors.foreach(neighbor => {
-            buttonMap(neighbor).setBorder(javax.swing.BorderFactory.createLineBorder(Color.RED, 2))
+            val currentButton = buttonMap(neighbor)
+            currentButton.setBorder(javax.swing.BorderFactory.createLineBorder(Color.RED, 2))
+            currentButton.setIsNeighbour(true)
           })
-        } else
-          resetButton()
-        btnState.setSelected(!btnState.isSelected)
+          btnState.setSelected(!btnState.isSelected)
+          btnState.setBorder(javax.swing.BorderFactory.createLineBorder(Color.BLACK, 2))
+        }
       })
+
       btnState.setBounds(posX.toInt, posY.toInt, 40, 40)
       panel.add(btnState)
       buttonMap += (name -> btnState)
     }
   }
-/*
-  // Aggiungi gli elementi alla GUI
-  val btnItaly = new JButton() {
-    //setBorder(BorderFactory.createEmptyBorder())
-    setContentAreaFilled(false) // Rimuove lo sfondo del bottone
-    setForeground(Color.BLACK) // Imposta il colore del testo
-    setFocusPainted(false) // Rimuove l'effetto di focuss
-    setText(9.toString)
-    setFont(new Font("Arial", 12, 10))
 
-    override def paintComponent(g: Graphics): Unit = {
-      val g2d = g.asInstanceOf[Graphics2D]
-      val center = new Point2D.Float(getWidth / 2.0f, getHeight / 2.0f)
-      val radius = Math.min(getWidth, getHeight) / 2.0f
-      val circle = new Ellipse2D.Float(center.x - radius, center.y - radius, 2.0f * radius, 2.0f * radius)
-      g2d.setColor(Color.YELLOW) // Imposta il colore del cerchio
-      g2d.fill(circle) // Disegna il cerchio
-      super.paintComponent(g) // Disegna il testo del bottone
-    }
-  }
-  btnItaly.setBounds(468, 270, 40, 40)
-
-  val btnFrance = new JButton() {
-    //setBorder(BorderFactory.createEmptyBorder())
-    setContentAreaFilled(false) // Rimuove lo sfondo del bottone
-    setForeground(Color.WHITE) // Imposta il colore del testo
-    setFocusPainted(false) // Rimuove l'effetto di focuss
-    setText(8.toString)
-    setFont(new Font("Arial", 12, 10))
-
-    override def paintComponent(g: Graphics): Unit = {
-      val g2d = g.asInstanceOf[Graphics2D]
-      val center = new Point2D.Float(getWidth / 2.0f, getHeight / 2.0f)
-      val radius = Math.min(getWidth, getHeight) / 2.0f
-      val circle = new Ellipse2D.Float(center.x - radius, center.y - radius, 2.0f * radius, 2.0f * radius)
-      g2d.setColor(Color.BLUE) // Imposta il colore del cerchio
-      g2d.fill(circle) // Disegna il cerchio
-      super.paintComponent(g) // Disegna il testo del bottone
-    }
-  }
-  btnFrance.setBounds(468, 200, 40, 40)
-
-  val btnSwisse = new JButton() {
-    //setBorder(BorderFactory.createEmptyBorder())
-    setContentAreaFilled(false) // Rimuove lo sfondo del bottone
-    setForeground(Color.BLACK) // Imposta il colore del testo
-    setFocusPainted(false) // Rimuove l'effetto di focuss
-    setText(4.toString)
-    setFont(new Font("Arial", 12, 10))
-
-    override def paintComponent(g: Graphics): Unit = {
-      val g2d = g.asInstanceOf[Graphics2D]
-      val center = new Point2D.Float(getWidth / 2.0f, getHeight / 2.0f)
-      val radius = Math.min(getWidth, getHeight) / 2.0f
-      val circle = new Ellipse2D.Float(center.x - radius, center.y - radius, 2.0f * radius, 2.0f * radius)
-      g2d.setColor(Color.GREEN) // Imposta il colore del cerchio
-      g2d.fill(circle) // Disegna il cerchio
-      super.paintComponent(g) // Disegna il testo del bottone
-    }
-  }
-  btnSwisse.setBounds(400, 260, 40, 40)
-
-  val btnBrazil = new JButton() {
-    //setBorder(BorderFactory.createEmptyBorder())
-    setContentAreaFilled(false) // Rimuove lo sfondo del bottone
-    setForeground(Color.BLACK) // Imposta il colore del testo
-    setFocusPainted(false) // Rimuove l'effetto di focuss
-    setText(4.toString)
-    setFont(new Font("Arial", 12, 10))
-
-    override def paintComponent(g: Graphics): Unit = {
-      val g2d = g.asInstanceOf[Graphics2D]
-      val center = new Point2D.Float(getWidth / 2.0f, getHeight / 2.0f)
-      val radius = Math.min(getWidth, getHeight) / 2.0f
-      val circle = new Ellipse2D.Float(center.x - radius, center.y - radius, 2.0f * radius, 2.0f * radius)
-      g2d.setColor(Color.GREEN) // Imposta il colore del cerchio
-      g2d.fill(circle) // Disegna il cerchio
-      super.paintComponent(g) // Disegna il testo del bottone
-    }
-  }
-  btnBrazil.setBounds(280, 430, 40, 40)
-
-  val btnArgentina = new JButton() {
-    //setBorder(BorderFactory.createEmptyBorder())
-    setContentAreaFilled(false) // Rimuove lo sfondo del bottone
-    setForeground(Color.BLACK) // Imposta il colore del testo
-    setFocusPainted(false) // Rimuove l'effetto di focuss
-    setText(4.toString)
-    setFont(new Font("Arial", 12, 10))
-
-    override def paintComponent(g: Graphics): Unit = {
-      val g2d = g.asInstanceOf[Graphics2D]
-      val center = new Point2D.Float(getWidth / 2.0f, getHeight / 2.0f)
-      val radius = Math.min(getWidth, getHeight) / 2.0f
-      val circle = new Ellipse2D.Float(center.x - radius, center.y - radius, 2.0f * radius, 2.0f * radius)
-      g2d.setColor(Color.GREEN) // Imposta il colore del cerchio
-      g2d.fill(circle) // Disegna il cerchio
-      super.paintComponent(g) // Disegna il testo del bottone
-    }
-  }
-  btnArgentina.setBounds(200, 430, 40, 40)
-
-  val btnChile = new JButton() {
-
-    //setBorder(BorderFactory.createEmptyBorder())
-    setContentAreaFilled(false) // Rimuove lo sfondo del bottone
-    setForeground(Color.BLACK) // Imposta il colore del testo
-    setFocusPainted(false) // Rimuove l'effetto di focuss
-    setText(4.toString)
-    setFont(new Font("Arial", 12, 10))
-
-    override def paintComponent(g: Graphics): Unit = {
-      val g2d = g.asInstanceOf[Graphics2D]
-      val center = new Point2D.Float(getWidth / 2.0f, getHeight / 2.0f)
-      val radius = Math.min(getWidth, getHeight) / 2.0f
-      val circle = new Ellipse2D.Float(center.x - radius, center.y - radius, 2.0f * radius, 2.0f * radius)
-      g2d.setColor(Color.GREEN) // Imposta il colore del cerchio
-      g2d.fill(circle) // Disegna il cerchio
-      super.paintComponent(g) // Disegna il testo del bottone
-    }
-  }
-  btnChile.setBounds(220, 500, 40, 40)
-
-  panel.add(btnItaly)
-  panel.add(btnFrance)
-  panel.add(btnSwisse)
-  panel.add(btnBrazil)
-  panel.add(btnChile)
-  panel.add(btnArgentina)
-
-  var isSelected = false
-
-  btnItaly.addActionListener((_: ActionEvent) => {
-    if(!btnItaly.isSelected) {
-      val neighbors: Set[String] = controller.getNeighbor("italy", controller.getCurrentPlayer())
-      neighbors.foreach(neighbor => {
-        buttonMap(neighbor).setBorder(javax.swing.BorderFactory.createLineBorder(Color.RED, 2))
-      })
-    } else
-      resetButton()
-    btnItaly.setSelected(!btnItaly.isSelected)
-  })
-
-  val buttonMap: Map[String, JButton] = Map(
-    "italy" -> btnItaly,
-    "france" -> btnFrance,
-    "swisse" -> btnSwisse,
-    "brazil" -> btnBrazil,
-    "argentina" -> btnArgentina,
-    "chile" -> btnChile
-  )
-  */
   val btnShowMyStates = new JButton() {
     setText("Show my States")
   }
@@ -274,12 +135,9 @@ object GameMapGui extends App {
   frame.setVisible(true)
 
   def resetButton(): Unit =
-    buttonMap.foreach((_, button) => button.setBorder(BorderFactory.createEmptyBorder()))
-    /*btnItaly.setBorder(BorderFactory.createEmptyBorder())
-    btnFrance.setBorder(BorderFactory.createEmptyBorder())
-    btnSwisse.setBorder(BorderFactory.createEmptyBorder())
-    btnChile.setBorder(BorderFactory.createEmptyBorder())
-    btnArgentina.setBorder(BorderFactory.createEmptyBorder())
-    btnBrazil.setBorder(BorderFactory.createEmptyBorder())
-*/
+    buttonMap.foreach((_, button) => {
+      button.setBorder(BorderFactory.createEmptyBorder())
+      button.setIsNeighbour(false)
+      button.setSelected(false)
+    })
 }
