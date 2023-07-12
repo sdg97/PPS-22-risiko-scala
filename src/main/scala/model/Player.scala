93decffe3619a93be8fd9f6cc8c83883d2609891
simon <simone.delgatto@studio.unibo.it>
2023-07-12 09:38:30 +0200

    assign state to player

--- src/main/scala/model/Player.scala
+++ src/main/scala/model/Player.scala
@@ -3,8 +3,12 @@ package model
 trait Player {
   def username: String
   def color: PlayerColor
-
 }
 
+object Player:
+  def apply(username: String, color: PlayerColor): Player =
+    new PlayerImpl(username, color)
 
+object M extends App:
+  Player("simone", PlayerColor.BLACK)
 
--- src/main/scala/model/State.scala
+++ src/main/scala/model/State.scala
@@ -7,3 +7,7 @@ trait State {
   def addWagon(numberOfWagonsToAdd: Int): Unit
   def removeWagon(numberOfWagonToRemove: Int): Unit
 }
+
+object State:
+  def apply(name: String, numberOfWagon: Int, player: Player): State =
+    new StateImpl(name, numberOfWagon, player)
