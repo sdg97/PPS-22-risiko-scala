b9cc33e95bb876e204ce34952cf7ad33494121df
simon <simone.delgatto@studio.unibo.it>
2023-07-24 20:16:40 +0200

    ok player

--- src/main/scala/view/component/CurrentPlayerComponent.scala
+++ src/main/scala/view/component/CurrentPlayerComponent.scala
@@ -3,6 +3,7 @@ package view.component
 import controller.ControllerModule.Controller
 import model.Player
 
+import java.awt.Color
 import javax.swing.{JLabel, JPanel}
 
 private[view] class CurrentPlayerComponent(c: Controller):
@@ -13,7 +14,10 @@ private[view] class CurrentPlayerComponent(c: Controller):
     panel
 
   def update() =
+    val label = JLabel(s"${LABEL_PREFIX} ${c.getCurrentPlayer().username}")
+    label.setForeground(Color.decode(c.getCurrentPlayer().color.rgb.toString))
     panel.removeAll()
-    panel add JLabel(s"${LABEL_PREFIX} ${c.getCurrentPlayer().username}")
-    panel.setSize(panel.getPreferredSize())
-    
+    panel setOpaque false
+    panel add label
+    panel setOpaque false
+    panel setSize panel.getPreferredSize()
