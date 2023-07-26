b716367b6a967a39d3d93a97d607f6a7f964122d
Pietro Lelli <pietro.lelli00@gmail.com>
2023-07-26 11:57:31 +0200

    add configuration test

---
+++ src/test/scala/model/ConfigurationTest.scala
@@ -0,0 +1,34 @@
+package model
+
+import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
+import org.junit.{Before, Test}
+import utils.SetupFromFiles
+
+import java.io.File
+import scala.io.Source
+
+class ConfigurationTest:
+
+  private val gameMap = new GameMap()
+
+  @Before
+  def before(): Unit =
+    SetupFromFiles.setup(gameMap)
+
+  @Test
+  def testAllStatesAreLoaded(): Unit =
+    val stateFile = new File("src/main/resources/config/states.txt")
+    val numLines = Source.fromFile(stateFile).getLines().size
+    assertEquals(gameMap.nodes.size, numLines)
+
+  @Test
+  def testAllBordersAreLoaded(): Unit =
+    val bordersFile = new File("src/main/resources/config/borders.txt")
+    val numLines = Source.fromFile(bordersFile).getLines().size
+    assertEquals(gameMap.edges.size, numLines)
+
+  @Test
+  def testAllContinentsAreLoaded(): Unit =
+    val continentFile = new File("src/main/resources/config/continents.txt")
+    val numLines = Source.fromFile(continentFile).getLines().size
+    assertEquals(gameMap.continents.size, numLines)
