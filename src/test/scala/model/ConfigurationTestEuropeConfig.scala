842cb2ce43d297d341719aa669ddb6acf653db04
Pietro Lelli <pietro.lelli00@gmail.com>
2023-08-08 11:06:07 +0200

    add test for europe configuration

--- src/test/scala/model/ConfigurationTestEuropeConfig.scala
+++ src/test/scala/model/ConfigurationTestEuropeConfig.scala
@@ -7,28 +7,28 @@ import utils.SetupFromFiles
 import java.io.File
 import scala.io.Source
 
-class ConfigurationTest:
+class ConfigurationTestEuropeConfig:
 
   private val gameMap = new GameMap()
 
   @Before
   def before(): Unit =
-    SetupFromFiles.setup(gameMap, VersionMap.Classic)
+    SetupFromFiles.setup(gameMap, VersionMap.Europe)
 
   @Test
-  def testAllStatesAreLoaded(): Unit =
-    val stateFile = new File("src/main/resources/config/states.txt")
+  def testAllStatesAreLoadedEuropeConfig(): Unit =
+    val stateFile = new File("src/main/resources/config/states_europe.txt")
     val numLines = Source.fromFile(stateFile).getLines().size
     assertEquals(gameMap.nodes.size, numLines)
 
   @Test
-  def testAllBordersAreLoaded(): Unit =
-    val bordersFile = new File("src/main/resources/config/borders.txt")
+  def testAllBordersAreLoadedEuropeConfig(): Unit =
+    val bordersFile = new File("src/main/resources/config/borders_europe.txt")
     val numLines = Source.fromFile(bordersFile).getLines().size
     assertEquals(gameMap.edges.size, numLines)
 
   @Test
-  def testAllContinentsAreLoaded(): Unit =
-    val continentFile = new File("src/main/resources/config/continents.txt")
+  def testAllContinentsAreLoadedEuropeConfig(): Unit =
+    val continentFile = new File("src/main/resources/config/continents_europe.txt")
     val numLines = Source.fromFile(continentFile).getLines().size
     assertEquals(gameMap.continents.size, numLines)
