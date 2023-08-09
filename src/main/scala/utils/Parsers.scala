ea7578717949666b2e0e92b9852f79f407893541
Pietro Lelli <pietro.lelli00@gmail.com>
2023-08-09 10:20:31 +0200

    add Parsers to setup from file

---
+++ src/main/scala/utils/Parsers.scala
@@ -0,0 +1,51 @@
+package utils
+
+import model.{Continent, State}
+
+object Parsers:
+  /**
+   * Allows to parse a string to an object of type T
+   *
+   * @tparam T the parser's return type
+   */
+  trait Parser[T]:
+    /**
+     * convert the given string in an object of type T if possible
+     *
+     * @param line the string to be converted
+     * @return an option containing the result if the parsing went well, an empty option otherwise
+     */
+    def parse(line: String): Option[T]
+
+  /**
+   * a parser to convert strings to State
+   */
+  object StateParser extends Parser[State] :
+    override def parse(line: String): Option[State] =
+      line.split(",").toList match {
+        case name :: posX :: posY :: Nil => Some(State(name.trim, 0, null, posX.trim.toInt, posY.trim.toInt))
+        case _ => None
+      }
+
+  /**
+   * a parser to convert strings to Borders (String, String)
+   */
+  object BorderParser extends Parser[(String,String)]:
+    override def parse(line: String): Option[(String,String)] =
+      line.split(",").toList match {
+        case state1 :: state2 :: Nil => Some((state1.trim, state2.trim))
+        case _ => None
+      }
+
+  /**
+   * a parser to convert strings to Continent
+   */
+  object ContinentParser extends Parser[Continent]:
+    override def parse(line: String): Option[Continent] =
+      line.split(",").toList match {
+        case continentName :: other if other.nonEmpty =>
+          val continentStates = other.dropRight(1).map(_.trim).toSet
+          val bonus = other.last.trim.toInt
+          Some(Continent(continentName.trim, continentStates, bonus))
+        case _ => None
+      }
--- src/main/scala/utils/SetupFromFiles.scala
+++ src/main/scala/utils/SetupFromFiles.scala
@@ -1,6 +1,7 @@
 package utils
 
 import model.{Continent, GameMap, State, VersionMap}
+import utils.Parsers.{BorderParser, ContinentParser, StateParser}
 
 import java.io.File
 import scala.io.Source
@@ -15,42 +16,15 @@ object SetupFromFiles:
   def setup(gameMap: GameMap, versionMap:VersionMap): Unit = {
     def readLinesFromFile(file: File): Seq[String] = Source.fromFile(file).getLines().toList
 
-    def parseState(stateLine: String): Option[State] =
-      stateLine.split(",").toList match {
-        case name :: posX :: posY :: Nil => Some(State(name.trim, 0, null, posX.trim.toInt, posY.trim.toInt))
-        case _ => None
-      }
-
-    def parseBorder(borderLine: String): Option[(String, String)] =
-      borderLine.split(",").toList match {
-        case state1 :: state2 :: Nil => Some((state1.trim, state2.trim))
-        case _ => None
-      }
-
-    def parseContinent(continentLine: String): Option[Continent] =
-      continentLine.split(",").toList match {
-        case continentName :: other if other.nonEmpty =>
-          val continentStates = other.dropRight(1).map(_.trim).toSet
-          val bonus = other.last.trim.toInt
-          Some(Continent(continentName.trim, continentStates, bonus))
-        case _ => None
-      }
-
-    def setTypeOfMap(): (String, String, String) = versionMap match
-      case typeMap if typeMap==(VersionMap.Classic) =>
-        ("src/main/resources/config/states.txt", "src/main/resources/config/borders.txt", "src/main/resources/config/continents.txt")
-      case typeMap if typeMap==(VersionMap.Europe) =>
-        ("src/main/resources/config/states_europe.txt", "src/main/resources/config/borders_europe.txt", "src/main/resources/config/continents_europe.txt")
+    def setTypeOfMap() = versionMap match
+      case VersionMap.Classic =>
+        (File("src/main/resources/config/states.txt"), File("src/main/resources/config/borders.txt"), File("src/main/resources/config/continents.txt"))
+      case VersionMap.Europe =>
+        (File("src/main/resources/config/states_europe.txt"), File("src/main/resources/config/borders_europe.txt"), File("src/main/resources/config/continents_europe.txt"))
       case _ => null
 
-    val resultVersionMap=setTypeOfMap()
-
-    val stateFile = new File(resultVersionMap._1)
-    val borderFile = new File(resultVersionMap._2)
-    val continentFile = new File(resultVersionMap._3)
-    
-
-    readLinesFromFile(stateFile).flatMap(parseState).foreach(gameMap.addNode)
-    readLinesFromFile(borderFile).flatMap(parseBorder).foreach { (state1, state2) => gameMap.addEdge(state1, state2) }
-    readLinesFromFile(continentFile).flatMap(parseContinent).foreach(gameMap.addContinent)
+    val resultVersionMap = setTypeOfMap()
+    readLinesFromFile(resultVersionMap._1).flatMap(StateParser.parse).foreach(gameMap.addNode)
+    readLinesFromFile(resultVersionMap._2).flatMap(BorderParser.parse).foreach { (state1, state2) => gameMap.addEdge(state1, state2) }
+    readLinesFromFile(resultVersionMap._3).flatMap(ContinentParser.parse).foreach(gameMap.addContinent)
   }
