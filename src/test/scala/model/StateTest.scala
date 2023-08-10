f3107001164cd8caa6a302d1777e03fe2433277a
Pietro Lelli <pietro.lelli00@gmail.com>
2023-08-10 15:18:54 +0200

    refactor state test

--- src/test/scala/model/StateTest.scala
+++ src/test/scala/model/StateTest.scala
@@ -6,20 +6,28 @@ import org.scalatest.matchers.should.Matchers
 import scala.util.Random
 
 class StateTest extends AnyFunSuite with Matchers:
-  val player = Player("player1", PlayerColor.YELLOW)
-  val state = State("Brasile", 10, player)
+  val player1: Player = Player("pietro", PlayerColor.YELLOW)
+  val player2: Player = Player("luca", PlayerColor.BLUE)
+
+  val state: State = State("italy", 5, player1)
 
   test("Test create State"){
-    assert(state.name=="Brasile")
-    assert(state.numberOfTanks.equals(10))
+    assert(state.name == "italy")
+    assert(state.numberOfTanks.equals(5))
+    assert(state.player == player1)
   }
 
   test("Test add wagon to State"){
     state.addTanks(2)
-    assert(state.numberOfTanks.equals(12))
+    assert(state.numberOfTanks.equals(7))
   }
 
   test("Test remove wagon to State"){
     state.removeTanks(2)
-    assert(state.numberOfTanks.equals(10))
+    assert(state.numberOfTanks.equals(3))
+  }
+
+  test("Test assign to player") {
+    state.setPlayer(player2)
+    assert(state.player == player2)
   }
